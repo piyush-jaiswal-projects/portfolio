67a91689292206caee8d79706f47d62e75b704d1
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-06-13 20:35:00 +0530

    Create Story Section

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,11 +1,12 @@
-import { Landing } from "../section";
+import { Landing, Connect, FeedbackForm, Story } from "../section";
 
 function About() {
     return (
         <>
-        <Landing type="page">
-            {/* Specific Child */}
-       </Landing>     
+            <Landing type="page" />
+            <Story />
+            <FeedbackForm title="Wanna say something?" desc="" />
+            <Connect />    
         </>
     )
 }
