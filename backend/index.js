6247bd9033fdf2853776c6489b0cb8e23e7ac5e1
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-06-17 15:49:04 +0530

    Portfolio Backend

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const { log } = require('console');
+const axios = require('axios')
+const url = 'https://dev.to/api/articles/me'
+const cors =  require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Allow Cross-Origin requests
+const corsOptions = {
+    origin: '*', // Allow all origins
+    methods: 'PUT, POST, GET, DELETE, PATCH, OPTIONS', // Allowed methods
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization', // Allowed headers
+    credentials: true,
+    maxAge: 800,
+  };
+  
+  app.use(cors(corsOptions));
+
+const port = 7001;
+const apiKey = "qgAaSN1FJw1RNtct8UF97t5t";
+
+
+app.get('/articles', (req, res) => {
+    axios.get(url, {
+        headers: {
+          'api-key': apiKey
+        }
+      })
+        .then(response => {
+          res.send(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+          res.status(500).json({ error: 'Error fetching articles' });
+        });
+});
+    
+
+app.listen(port, () => {
+    console.log("Server Started On " + port);
+})
+
