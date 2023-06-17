f904eea3ccf05122ad1d5b43b40884f9b2112ee0
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-06-17 21:33:41 +0530

    Blog Data

--- client/src/components/blog-card.jsx
+++ client/src/components/blog-card.jsx
@@ -1,20 +1,20 @@
-import { Project1 } from "../assets";
+// import { Project1 } from "../assets";
 
 export default function BlogCard(props) {
-    var image;
-    switch (props.blog.imageName) {
-        case "Project1":
-            image = Project1
-            break;
-        default:
-            image = Project1
-            break;
-    }
+    // var image;
+    // switch (props.blog.imageName) {
+    //     case "Project1":
+    //         image = Project1
+    //         break;
+    //     default:
+    //         image = Project1
+    //         break;
+    // }
     return (
-        <div key={props.blog.id} className="bg-darkblueTwo w-[80vw] mx-auto my-8 py-2 md:py-0 flex flex-wrap shadow-lg">
+        <div key={props.blog.id} className="bg-darkblueTwo w-[80vw] mx-auto my-8 py-2 md:py-0 flex flex-wrap items-center shadow-lg">
             
             <div className="my-4 w-[90%] mx-auto md:m-4 md:w-[30%] rounded-md">
-                <img className="object-cover rounded-md" src={image} alt={props.blog.title}></img>
+                <img className="object-cover rounded-md" src={props.blog.image} alt={props.blog.title}></img>
             </div>
 
             <div className="mx-4 md:m-4 w-[90%] md:w-[60%]">
--- client/src/pages/blogpage.js
+++ client/src/pages/blogpage.js
@@ -1,17 +1,20 @@
 import { useParams } from "react-router-dom"
 import { Landing } from "../section";
 import { blogs } from "../data";
+import ReactMarkdown from "react-markdown";
 
 export default function BlogPage() {
 
     const id = useParams().blogId;
-    const blog = blogs.filter((obj)=> obj.id === Number(id))[0]
+    const blog = blogs.filter((obj) => obj.id === Number(id))[0]
     return (
         <>
             <Landing type="blog" title={blog.title} desc={blog.desc.substring(1, 20)} />
             
-            <div className="w-[90vw] mx-auto my-4 text-[20px] leading-tight">
+            <div className="text-lightblue w-[90vw] mx-auto my-4 text-[20px] leading-tight">
+                <ReactMarkdown>
                 {blog.content}
+                </ReactMarkdown>
             </div>
         </>
     )
